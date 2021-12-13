0bc1232507c3cf75fb5cd8f74a73f24b142af5bf
sneakychees3 <sneakycheese3@gmail.com>
2021-12-12 20:48:19 -0500

    Changed input system of controller
    Won't change code anymore this is perfect.

--- GunNinjaWIP/Assets/Scripts/playerMovement/newPlayerMovement.cs
+++ GunNinjaWIP/Assets/Scripts/playerMovement/newPlayerMovement.cs
@@ -1,11 +1,14 @@
-   
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class betterPlayerMovement : MonoBehaviour{
-    //horizontal movement stuff
+public class newPlayerMovement : MonoBehaviour
+{
+    //components
+    [SerializeField]Rigidbody2D rb;
+    Collisions colls;
+    [Header("Movement Stuff")]
     Vector2 direction;
     [SerializeField] float maxSpeed=10f;
     [SerializeField] float acc=9f;
@@ -13,41 +16,39 @@ public class betterPlayerMovement : MonoBehaviour{
     [SerializeField] float airAcc=1f;
     [SerializeField] float airDecel=1f;
     [SerializeField] float accPower=1.2f;
-    [SerializeField] float hFriction=.85f;
+    [SerializeField] float horizontalFriction=.85f;
+    
+    [Space(10)]
+    [Header("Jump Stuff")]
     [SerializeField] float jumpForce=12;
     [SerializeField] float jumpCutAmount=0.5f;
     [SerializeField] float gravityScale=1;
     [SerializeField] float fallMultiplier=2f;
-    bool isJumping;
-    //components
-     Rigidbody2D rb;
-     betterJump jumpScript;
-     Collisions colls;
-    //timers
+    [Space(10)]
+    [Header("Timers")]
     [SerializeField] float lastGroundedTime=0.1f;
     float lastGrounded=0;
     [SerializeField] float lastJumpPressedTime=0.1f;
     float lastJumpPressed=0;
-
-    void Start() {
+    //other bools
+    bool isJumping=false;
+    bool jumpReleased=false;
+    bool jumpPressed=false;
+    void Start(){
         rb=this.GetComponent<Rigidbody2D>();
-        jumpScript=this.GetComponent<betterJump>();
         colls=this.GetComponent<Collisions>();
     }
+
     void Update() {
-        direction=new Vector2((Input.GetAxisRaw("Horizontal")),(Input.GetAxisRaw("Vertical")));
-        if(rb.velocity.y<0)
-        {
-            isJumping=false;
-        }
         //spacebar timer
         if(lastJumpPressed<0){
             lastJumpPressed=0;
         }else{
             lastJumpPressed-=Time.deltaTime;
         }
-        if(Input.GetButtonDown("Jump")){
+        if(jumpPressed){
             lastJumpPressed=lastJumpPressedTime;
+            jumpPressed=false;
         }
         //ground timer
         if(lastGrounded<0){
@@ -57,26 +58,35 @@ public class betterPlayerMovement : MonoBehaviour{
         }
         if(colls.onGround){
             lastGrounded=lastGroundedTime;
-            jumpScript.enabled=false;
         }
-        
     }
-    void FixedUpdate() {
-        horizontalMove(direction.x);  
-         if(lastGrounded>0&&lastJumpPressed>0&&!isJumping){
-        jump(direction,false);
+    void FixedUpdate(){
+        applyMovement(direction.x);
+        if(lastGrounded>0&&lastJumpPressed>0&&!isJumping){
+            applyJump();
+        }
+        if(rb.velocity.y>0&&isJumping&&jumpReleased){
+            rb.AddForce(Vector2.down*rb.velocity.y*jumpCutAmount,ForceMode2D.Impulse);
+        }
+        if(rb.velocity.y<0){
+            rb.gravityScale=gravityScale*fallMultiplier;
+        }
+        else{
+            rb.gravityScale=gravityScale;
+        }
+        resetBools();
     }
-         
-         //faster fall
-         if(rb.velocity.y<0){
-             rb.gravityScale=gravityScale*fallMultiplier;
-         }
-         else{
-             rb.gravityScale=gravityScale;
-         }
+    public void Move(InputAction.CallbackContext context){
+        direction=context.ReadValue<Vector2>();
     }
-    void horizontalMove(float x){
-         if(Mathf.Abs(rb.velocity.y)<=0){
+     public void Jump(InputAction.CallbackContext context){
+        jumpPressed=true;
+        if(context.canceled&&rb.velocity.y>0&&!jumpReleased){
+            jumpReleased=true;
+        }
+    }
+    public void applyMovement(float x){
+        if(Mathf.Abs(rb.velocity.y)<=0){
         float targetSpeed=x*maxSpeed;// calculate the max speed in desired direction
         float speedDiff=targetSpeed-rb.velocity.x;// difference between current speed and max speed
         float accRate=(Mathf.Abs(targetSpeed)>0.01f)?acc:decc; // get the rate needed to reach top speed 
@@ -93,21 +103,24 @@ public class betterPlayerMovement : MonoBehaviour{
         }
         //adding friction
         if(lastGrounded>0&&Mathf.Abs(x)<0.1) {
-            float temp=Mathf.Min(Mathf.Abs(rb.velocity.x),Mathf.Abs(hFriction));
+            float temp=Mathf.Min(Mathf.Abs(rb.velocity.x),Mathf.Abs(horizontalFriction));
             temp*=Mathf.Sign(rb.velocity.x);
             rb.AddForce(Vector2.right*-temp,ForceMode2D.Impulse);
         }
     }
-    void jump(Vector2 dir,bool wall) {
-        lastGrounded=0;
+   public void applyJump(){
+       lastGrounded=0;
         lastJumpPressed=0;
         rb.velocity=new Vector2(rb.velocity.x,0);
         rb.AddForce(Vector2.up*jumpForce,ForceMode2D.Impulse);
         isJumping=true;
-    }
-    void onJumpUp(){
-        if(rb.velocity.y>0&&isJumping){
-            rb.AddForce(Vector2.down*rb.velocity.y*jumpCutAmount,ForceMode2D.Impulse);
+   }
+    public void resetBools(){
+        if(colls.onGround&&rb.velocity.y==0){
+            isJumping=false;
+            jumpReleased=false;
+            jumpPressed=false;
+            rb.gravityScale=gravityScale;
         }
     }
 }
