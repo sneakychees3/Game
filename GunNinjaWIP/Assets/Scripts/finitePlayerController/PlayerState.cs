21677eac9556fec94cef8fc280f31e2a08b25a80
sneakychees3 <sneakycheese3@gmail.com>
2021-12-16 23:06:36 -0500

    forgot to save

--- GunNinjaWIP/Assets/Scripts/finitePlayerController/PlayerState.cs
+++ GunNinjaWIP/Assets/Scripts/finitePlayerController/PlayerState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     //This class is the base class for all of our possible player states
+    //any state we make will inheret from this class
     void Start()
     {
         
