29639ba60ebe43f93870cfe430bc4a81c90d1835
sneakychees3 <sneakycheese3@gmail.com>
2021-12-16 23:06:10 -0500

    made some scripts, im tired so ill do it tomm

---
+++ GunNinjaWIP/Assets/Scripts/finitePlayerController/PlayerState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    //This class is the base class for all of our possible player states
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
