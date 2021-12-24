0eb5069d099e280c6f8b978e8901a118b1e1f4f0
sneakychees3 <sneakycheese3@gmail.com>
2021-12-23 21:16:55 -0500

    started putting together the state machine
    need to tweak the player class a bit then ill add states starting with idle and run

--- GunNinjaWIP/Assets/Scripts/State Machine/player.cs
+++ GunNinjaWIP/Assets/Scripts/State Machine/player.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public Rigidbody2D rb{get;private set;}
+    [SerializeField] public playerData pd;
+    public stateMachine stm{get;private set;}
+    
+    #region conditions
+    Vector2 currentV=Vector2.zero;
+    public float collisionRadius = 0.25f;
+    public Vector2 bottomOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.red;
+    public LayerMask groundLayer;
+    #endregion
+    #region states
+    
+    #endregion
+    void initializeStates(){
         
+        //always initialize new states here 
+    }
+    void Awake() {
+        rb=this.GetComponent<Rigidbody2D>();
+        stm=this.GetComponent<stateMachine>();
+        initializeStates();
+        //stm.Initialize(idle);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        stm.currentState.logic();
+        currentV=rb.velocity;
     }
+    void FixedUpdate() {
+        stm.currentState.physics();
+    }
+    #region conditions
+    public bool isMovingX(){
+        return Mathf.Abs(currentV.x)>0.01f;
+    }
+    public bool isGrounded(){
+        return Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+    }
+    public int isTouchingWall(){
+        bool onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+        bool onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+
+        return onRightWall ? -1 : 1;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        var positions = new Vector2[] { bottomOffset, rightOffset, leftOffset };
+
+        Gizmos.DrawWireSphere((Vector2)transform.position  + bottomOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+    }
+    #endregion
 }
--- GunNinjaWIP/Assets/Scripts/State Machine/playerBase.cs
+++ GunNinjaWIP/Assets/Scripts/State Machine/playerBase.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public abstract class playerBase
 {
+    #region components
     protected Rigidbody2D rb;
     [SerializeField] playerData pd;
     protected Collisions colls;
     protected Animator anim;
     protected player player;
     protected stateMachine stm;
+    #endregion
     
+    #region input getters
+    public Vector2 direction { get; private set; }
+    public float lastJumpPressed { get; private set; }
+    public bool jumpReleased { get; private set; }
+    #endregion 
+    #region constructor
     public playerBase(player player,stateMachine stm){
         this.player=player;
         this.stm=stm;
@@ -18,7 +26,32 @@ public abstract class playerBase
         rb=player.GetComponent<Rigidbody2D>();
         colls=player.GetComponent<Collisions>();
     }
+    #endregion
+    private void Start() {
+        lastJumpPressed=0;
+        jumpReleased=false;
+    }
+    
 
+    #region inputs
+    public void Move(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            lastJumpPressed = player.pd.lastJumpPressedTime;
+        }
+
+        if (context.canceled && player.rb.velocity.y > 0 && !jumpReleased)
+        {
+            jumpReleased = true;
+        }
+    }
+    #endregion
+    
     public virtual void Enter(){
 
     }
