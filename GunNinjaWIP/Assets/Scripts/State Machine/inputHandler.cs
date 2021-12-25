758fc6e693f29c1c0d236faaaae1e04744dba200
sneakychees3 <sneakycheese3@gmail.com>
2021-12-25 17:35:42 -0500

    did a bit more work on it
    I got the state machine to switch from state to state but I need to get them transitioning better

--- GunNinjaWIP/Assets/Scripts/State Machine/inputHandler.cs
+++ GunNinjaWIP/Assets/Scripts/State Machine/inputHandler.cs
@@ -9,29 +9,27 @@ public class inputHandler : MonoBehaviour
     public Vector2 direction { get; private set; }
     public float lastJumpPressed;
     public bool jumpReleased; 
-    [SerializeField] playerBase player;
+    [SerializeField] player player;
     void Start()
     {
+        player=this.GetComponent<player>();
         jumpReleased = false;
         lastJumpPressed = 0;
     }
     public void Move(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
-        Debug.Log(direction);
     }
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.started)
         {
             lastJumpPressed = player.pd.lastJumpPressedTime;
-            Debug.Log("jump button pressed");
         }
 
         if (context.canceled && player.rb.velocity.y > 0 && !jumpReleased)
         {
             jumpReleased = true;
-            Debug.Log("jump button let go");
         }
     }
     
--- GunNinjaWIP/Assets/Scripts/State Machine/states/jumpState.cs
+++ GunNinjaWIP/Assets/Scripts/State Machine/states/jumpState.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class jumpState : playerBase
 {
+    public bool isJumping=false;
     public jumpState(player player, stateMachine stm) : base(player, stm)
     {
     }
@@ -11,7 +12,8 @@ public class jumpState : playerBase
     public override void Enter()
     {
         base.Enter();
-        Debug.Log("jump");
+        Debug.Log("jumpState");
+        applyJump();
     }
 
     public override void Exit()
@@ -29,4 +31,11 @@ public class jumpState : playerBase
         base.physics();
     }
 
+    public void applyJump(){
+        lastGrounded=0;
+        inputs.lastJumpPressed=0;
+        rb.velocity=new Vector2(rb.velocity.x,0);
+        rb.AddForce(Vector2.up*player.pd.jumpForce,ForceMode2D.Impulse);
+        isJumping=true;
+   }
 }
